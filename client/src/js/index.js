635f5c9c338f1899401b77467828e902c6d5e6db
Ray Dixon <raymondgdixonii@gmail.com>
2024-02-21 14:57:56 -0500

    final

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,5 +1,7 @@
 import { Workbox } from 'workbox-window';
 import Editor from './editor';
+import './database';
+import '../css/style.css';
 
 const main = document.querySelector('#main');
 main.innerHTML = '';
@@ -8,44 +10,24 @@ const loadSpinner = () => {
   const spinner = document.createElement('div');
   spinner.classList.add('spinner');
   spinner.innerHTML = `
-    <div class="loading-container">
-      <div class="loading-spinner"></div>
-    </div>
+  <div class="loading-container">
+  <div class="loading-spinner" />
+  </div>
   `;
   main.appendChild(spinner);
 };
 
-const initializeEditor = async () => {
-  try {
-    // Initialize the editor
-    const editor = new Editor();
-    if (!editor) {
-      loadSpinner();
-    }
-  } catch (error) {
-    console.error('Error initializing editor:', error);
-    // Fallback to loading spinner if editor initialization fails
-    loadSpinner();
-  }
-};
+const editor = new Editor();
+
+if (typeof editor === 'undefined') {
+  loadSpinner();
+}
 
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', async () => {
-    // Register workbox service worker
-    try {
-      const workboxSW = new Workbox('/src-sw.js');
-      await workboxSW.register();
-      console.log('Service worker registered successfully.');
-    } catch (error) {
-      console.error('Error registering service worker:', error);
-    }
-
-    // Initialize the editor after service worker registration
-    initializeEditor();
-  });
+  // register workbox service worker
+  const workboxSW = new Workbox('/src-sw.js');
+  workboxSW.register();
 } else {
   console.error('Service workers are not supported in this browser.');
-  // Initialize the editor without service worker support
-  initializeEditor();
 }
